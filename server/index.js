585920de7452d6caeead3d884b3a4a0170ebe99f
yuyuyu2118 <yuyuyu2118@gmail.com>
2024-04-19 15:06:25 +0900

    refactor/server-index: dotenvインポートとインデント修正

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import 'dotenv/config';
 import http from "http";
 import express from "express";
 
@@ -8,8 +8,8 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    res.send("Hello World!");
+})
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
